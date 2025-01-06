965b388e8d5f5d3c00f4abe94a764322050399f1
hacksaw2 <153176276+hacksaw2@users.noreply.github.com>
2025-01-07 01:05:58 +0530

    final changes

--- src/Centres.jsx
+++ src/Centres.jsx
@@ -30,10 +30,10 @@ function Centres(){
     
              <div className="element" >
         <div className="table   w-[80vw] p-1 ">
-          <div className="container grid-item bg-blue-400 rounded-xl h-12 p-3 text-center 
+          <div className="container grid-item bg-blue-400 rounded-xl h-12 p-2 text-center 
             text-2xl font-bold text-gray-700">Name</div>
-          <div className="container2 grid-item bg-orange-400 h-12 rounded-xl p-3 text-center text-2xl font-bold text-gray-700">Place</div>
-          <div className="container3 grid-item bg-pink-400 h-12 rounded-xl p-3 text-center text-2xl font-bold text-gray-700">State</div>
+          <div className="container2 grid-item bg-orange-400 h-12 rounded-xl p-2 text-center text-2xl font-bold text-gray-700">Place</div>
+          <div className="container3 grid-item bg-pink-400 h-12 rounded-xl p-2 text-center text-2xl font-bold text-gray-700">State</div>
         </div>
         </div>
         
@@ -55,7 +55,7 @@ function Centres(){
           <div className="container hero grid-item bg-blue-400 rounded-xl h-12 p-3 text-center 
            text-[0.6rem] lg:text-[0.8rem] overflow-auto ">{element.name}</div>
           <div className="container2 grid-item bg-orange-400 h-12 rounded-xl p-3 text-center">{element.Place}</div>
-          <div className="container3 grid-item bg-pink-400 h-12 rounded-xl p-3 text-center text-[0.8rem] ">{element.State}</div>
+          <div className="container3 grid-item bg-pink-400 h-12 rounded-xl p-3 text-center text-[0.8rem] overflow-auto">{element.State}</div>
         </div>
         </div>
           })
--- src/Satellites.jsx
+++ src/Satellites.jsx
@@ -31,7 +31,7 @@ Sat();
     <div className=' flex flex-wrap gap-4 justify-center mt-4'>
      
      {sat.map((element)=>{
-      return <div className="x card bg-orange-500 mb-2 h-44 w-40 rounded-lg text-[0.7rem] " key={element.id}>
+      return <div className="x card bg-orange-500 mb-2 h-44 w-40 rounded-lg text-[0.7rem] shadow-xl shadow-gray-500 hover:bg-orange-600 hover:w-44 hover:h-48 transition-all duration-400" key={element.id}>
         <img className='p-1 rounded-lg' src='https://indbiz.gov.in/wp-content/uploads/2022/03/iStock-181075945-1.jpg' />
         
         <div className="1 flex ml-1 text-blue-600"><h1>Name:</h1>{element.id}</div>
